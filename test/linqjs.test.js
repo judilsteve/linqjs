a83ae67ce9a5d42e0deac296aaacab1ea4e34111
james <judilsteve>
2021-03-06 13:05:58 +0800

    Add tests for Any

--- test/linqjs.test.js
+++ test/linqjs.test.js
@@ -425,3 +425,27 @@ test('LastOrDefault', () => {
         .lastOrDefault(x => x.val - 2 === 0, 'default'))
     .toStrictEqual({ val: 2, id: 3 });
 });
+
+// Any
+
+test('Any: No elements, no predicate', () => {
+    expect([].any()).toBe(false);
+});
+
+test('Any: No elements', () => {
+    expect([].any(x => x === 42)).toBe(false);
+});
+
+test('Any: No predicate', () => {
+    expect([1,2,3].any()).toBe(true);
+});
+
+test('Any: No match', () => {
+    expect([1,2,3].any(x => x === 42)).toBe(false);
+});
+
+test('Any', () => {
+    expect([{val: 1}, {val: 2}, {val: 42}, null].any(x => x.val === 42)).toBe(true);
+});
+
+// All
