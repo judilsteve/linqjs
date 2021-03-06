72edc22718f0938cc11540d233414cc23079b074
james <judilsteve>
2021-03-06 11:05:04 +0800

    Tests for Single

--- test/linqjs.test.js
+++ test/linqjs.test.js
@@ -15,7 +15,7 @@ test('First: No elements', () => {
 });
 
 test('First', () => {
-    expect([1,2,3].first(x => x - 2 == 0)).toBe(2);
+    expect([1,2,3].first(x => x - 2 === 0)).toBe(2);
 });
 
 // FirstOrDefault
@@ -50,4 +50,42 @@ test('FirstOrDefault: No elements', () => {
 
 test('FirstOrDefault', () => {
     expect([1,2,3].firstOrDefault(x => x - 2 == 0, 'deafult')).toBe(2);
+});
+
+// Single
+
+test('Single: No elements, no predicate', () => {
+    expect(() => [].single()).toThrow('Sequence contained no elements');
+});
+
+test('Single: No predicate, one element', () => {
+    expect([42].single()).toBe(42);
+});
+
+test('Single: No predicate, multiple elements', () => {
+    expect(() => [42, 52].single()).toThrow('Sequence contained more than one element');
+});
+
+test('Single: No elements', () => {
+    expect(() => [].single(x => x === 5)).toThrow('Sequence contained no elements');
+});
+
+test('Single: One element, one match', () => {
+    expect([42].single(x => x - 2 === 40)).toBe(42);
+});
+
+test('Single: One element, no matches', () => {
+    expect(() => [32].single(x => x - 2 === 40)).toThrow('Sequence contained no elements');
+});
+
+test('Single: Multiple elements, no matches', () => {
+    expect(() => [32, 52].single(x => x - 2 === 40)).toThrow('Sequence contained no elements');
+});
+
+test('Single: Multiple elements, one match', () => {
+    expect([22, 42, 32].single(x => x - 2 === 40)).toBe(42);
+});
+
+test('Single: Multiple elements, multiple matches', () => {
+    expect(() => [52, 42, 42].single(x => x - 2 === 40)).toThrow('Sequence contained more than one element');
 });
