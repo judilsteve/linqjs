9b3595c9c9de419682c530dcbf564865aeda8387
james <judilsteve>
2021-03-06 17:56:50 +0800

    Tests/bug fixes for linqJoin

--- src/linqjs.js
+++ src/linqjs.js
@@ -478,26 +478,28 @@ registerIterableExtension("takeWhile", takeWhile);
 function* linqJoin(iterable, other, keyProjection, otherKeyProjection, resultProjection) {
     // If we can determine the lengths of the sequences
     // we can optimise memory usage by making our temporary data structure out of the smaller one
-    let smaller, larger, smallerKeyProjection, largerKeyProjection;
+    let smaller, larger, smallerKeyProjection, largerKeyProjection, orderedResultProjection;
     if(iterable.length !== undefined && other.length !== undefined && iterable.length > other.length) {
         smaller = other;
         larger = iterable;
         smallerKeyProjection = otherKeyProjection;
         largerKeyProjection = keyProjection;
         // Need to swap these
-        resultProjection = (a, b) => resultProjection(b, a);
+        orderedResultProjection = (a, b) => resultProjection(b, a);
     } else {
         smaller = iterable;
         larger = other;
         smallerKeyProjection = keyProjection;
         largerKeyProjection = otherKeyProjection;
+        orderedResultProjection = resultProjection;
     }
 
     const grouped = smaller.groupBy(smallerKeyProjection);
     for(const element of larger) {
-        const matching = grouped.get(largerKeyProjection(element));
-        for(const matchingElement of matching) {
-            yield resultProjection(matchingElement, element);
+        const key = largerKeyProjection(element);
+        if(!grouped.has(key)) continue;
+        for(const matchingElement of grouped.get(key)) {
+            yield orderedResultProjection(matchingElement, element);
         }
     }
 }
