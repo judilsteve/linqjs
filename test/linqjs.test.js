1deb5694f2f3c9ddfdbd81064cc818f5a57e662f
james <judilsteve>
2021-03-06 16:22:17 +0800

    Tests for Intersect

--- test/linqjs.test.js
+++ test/linqjs.test.js
@@ -676,4 +676,15 @@ test('Distinct', () => {
 });
 
 // Intersect
-// TODO Remember there are three distinct code paths for this function
+
+test('Intersect: Single sequence', () => {
+    expectAsArray([1,1,2,3].intersect()).toStrictEqual([1,2,3]);
+});
+
+test('Intersect: Two sequences', () => {
+    expectAsArray([1,1,2,3].intersect([1,3,5])).toStrictEqual([1,3]);
+});
+
+test('Intersect', () => {
+    expectAsArray([1,1,2,3].intersect([1,3,5], [1,5,6])).toStrictEqual([1]);
+});
