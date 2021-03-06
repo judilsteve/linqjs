5225faacdf991d647f5f027bb98b5f71a7154570
james <judilsteve>
2021-03-06 12:10:27 +0800

    Tests for skip

--- test/linqjs.test.js
+++ test/linqjs.test.js
@@ -248,7 +248,40 @@ test('Concat', () => {
 
 // Skip
 
-// TODO_JU Test both arrays (direct access) and others
 function* generate(array) {
     yield* array;
 }
+
+test('Skip: Direct access', () => {
+    expectAsArray([1,2,3,4,5].skip(2)).toStrictEqual([3,4,5]);
+});
+
+test('Skip: Direct access, skip whole sequence', () => {
+    expectAsArray([1,2,3,4,5].skip(5)).toStrictEqual([]);
+});
+
+test('Skip: Direct access, skip more than whole sequence', () => {
+    expectAsArray([1,2,3,4,5].skip(100)).toStrictEqual([]);
+});
+
+test('Skip: Direct access, empty sequence', () => {
+    expectAsArray([].skip(100)).toStrictEqual([]);
+});
+
+test('Skip', () => {
+    expectAsArray(generate([1,2,3,4,5]).skip(2)).toStrictEqual([3,4,5]);
+});
+
+test('Skip: Skip whole sequence', () => {
+    expectAsArray(generate([1,2,3,4,5]).skip(5)).toStrictEqual([]);
+});
+
+test('Skip: Skip more than whole sequence', () => {
+    expectAsArray(generate([1,2,3,4,5]).skip(100)).toStrictEqual([]);
+});
+
+test('Skip: Empty sequence', () => {
+    expectAsArray(generate([]).skip(100)).toStrictEqual([]);
+});
+
+// Take
