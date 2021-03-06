68b63ecf7b882685f34e236f146d7ea0f4271688
james <judilsteve>
2021-03-06 16:27:31 +0800

    Tests for Except.

--- test/linqjs.test.js
+++ test/linqjs.test.js
@@ -688,3 +688,15 @@ test('Intersect: Two sequences', () => {
 test('Intersect', () => {
     expectAsArray([1,1,2,3].intersect([1,3,5], [1,5,6])).toStrictEqual([1]);
 });
+
+// Except
+
+test('Except: No sequences', () => {
+    expectAsArray([1,1,2,3].except()).toStrictEqual([1,1,2,3]);
+});
+
+test('Except', () => {
+    expectAsArray([1,1,2,3,4,5,6,7,6,8].except([1,3], [6,5])).toStrictEqual([2,4,7,8]);
+});
+
+// OrderBy
