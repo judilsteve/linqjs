e08d11d6993bff4251683d6e7a3a1f54432ab52e
james <judilsteve>
2021-03-06 10:57:18 +0800

    Beginning to add unit tests.

--- test/linqjs.test.js
+++ test/linqjs.test.js
@@ -1,9 +1,53 @@
 require('../src/linqjs');
 
+// First
+
+test('First: No elements, no predicate', () => {
+    expect(() => [].first()).toThrow('Sequence contained no elements');
+});
+
 test('First: No predicate', () => {
     expect([1,2,3].first()).toBe(1);
 });
 
-test('Aggregate: Accumulator only', () => {
-    expect([1,2,3].aggregate((sum, next) => (sum ?? 0) + next)).toBe(6);
+test('First: No elements', () => {
+    expect(() => [].first(x => x === 5)).toThrow('Sequence contained no elements');
+});
+
+test('First', () => {
+    expect([1,2,3].first(x => x - 2 == 0)).toBe(2);
+});
+
+// FirstOrDefault
+
+test('FirstOrDefault: No elements, no predicate, no default value', () => {
+    expect([].firstOrDefault()).toBe(undefined);
+});
+
+test('FirstOrDefault: No predicate, no default value', () => {
+    expect([1,2,3].firstOrDefault()).toBe(1);
+});
+
+test('FirstOrDefault: No elements, no default value', () => {
+    expect([].firstOrDefault(x => x === 5)).toBe(undefined);
+});
+
+test('FirstOrDefault: No default value', () => {
+    expect([1,2,3].firstOrDefault(x => x - 2 == 0)).toBe(2);
+});
+
+test('FirstOrDefault: No elements, no predicate', () => {
+    expect([].firstOrDefault(null, 'default')).toBe('default');
+});
+
+test('FirstOrDefault: No predicate', () => {
+    expect([1,2,3].firstOrDefault(null, 'default')).toBe(1);
+});
+
+test('FirstOrDefault: No elements', () => {
+    expect([].firstOrDefault(x => x === 5, 'default')).toBe('default');
+});
+
+test('FirstOrDefault', () => {
+    expect([1,2,3].firstOrDefault(x => x - 2 == 0, 'deafult')).toBe(2);
 });
