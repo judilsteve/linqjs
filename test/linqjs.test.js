706f55eb12aba93c8ae8d196582a6fa0c37e7599
james <judilsteve>
2021-03-06 15:24:12 +0800

    Tests for ToSet.

--- test/linqjs.test.js
+++ test/linqjs.test.js
@@ -615,3 +615,19 @@ test('ToMap', () => {
 });
 
 // ToSet
+
+test('ToSet: No elements, no projection', () => {
+    expectAsArray([].toSet()).toStrictEqual([]);
+});
+
+test('ToSet: No projection', () => {
+    expectAsArray([1,2,3,4,3].toSet()).toStrictEqual([1,2,3,4]);
+});
+
+test('ToSet: No elements', () => {
+    expectAsArray([].toSet(x => x + 1)).toStrictEqual([]);
+});
+
+test('ToSet', () => {
+    expectAsArray([1,2,3,4,3].toSet(x => x + 1)).toStrictEqual([2,3,4,5]);
+});
